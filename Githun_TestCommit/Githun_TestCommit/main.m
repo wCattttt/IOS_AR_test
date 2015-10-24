5d43672c3f0f408195b056247deee0ca523435e9
imac <imac@imacdeiMac-10.local>
2015-10-24 11:07:54 +0800

    第一次提交Github代码

---
+++ Githun_TestCommit/Githun_TestCommit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Githun_TestCommit
+//
+//  Created by imac on 15/10/24.
+//  Copyright (c) 2015年 imac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Github Test");
+    }
+    return 0;
+}
